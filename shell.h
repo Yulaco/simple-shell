1899765cbf3c736d6bac31ef43ed8839c40dfd65
Yulaco <cleochen93@gmail.com>
2023-12-05 21:57:54 -0800

    Simple shell header

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_HEADER_
+#define _SHELL_HEADER_
+
+/*Standard Libraries*/
+#include <unistd.h>
+#include <stdio.h>
+
+
+/*Shell Functions*/
+int main(void);
+int isatty(int fd);
+
+#endif
