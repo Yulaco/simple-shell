d19fc5463984abe4e259273e1d9aa1cd71246d22
Yulaco <cleochen93@gmail.com>
2023-12-11 12:02:51 -0800

    Print

--- print_text.c
+++ print_text.c
@@ -13,3 +13,21 @@ ssize_t print_text(int fd, const void *dados, size_t count)
 {
 	return (write(STDOUT_FILENO, dados, frase_len(count)));
 }
+
+/**
+ * puts_text - Writes string to stdout.
+ * @s: Pointer to string.
+ *
+ * Return: On success non-negative value, else on error cast to EOF.
+ */
+int puts_text(const char *s)
+{
+	int words = 0;
+
+	do {
+		write(1, &s[i], 1);
+		words++;
+	} while (s[words] != '\0');
+
+	return (0);
+}
